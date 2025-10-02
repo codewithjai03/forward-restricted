194eabf297bcf7584988abc196d8eb888825e155
jai kumar <156773655+codewithjai03@users.noreply.github.com>
2025-10-02 08:18:12 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("6623123259", default=None, cast=int)
+API_HASH = config("AAGrWBfyUP9IFTibaUKaSZZ4EyRbqu2JEcI", default=None)
+BOT_TOKEN = config("6623123259:AAGrWBfyUP9IFTibaUKaSZZ4EyRbqu2JEcI", default=None)
+SESSION = config("BQFqIokApZRNc_9QqhwgZz1buZ6VaCZNOkGt2VsVCFTJzD1a-lOchN5ElB7CsfwkHdVNiqZ57aIvcb6kl_yr9rAEGVzjRdAlGQHIoz9xwv7J8lMAdMPwHjbdZ0BX2uZDNlyrU_yTJD91joQenddNcRTcbo9CH5ze34ipsrU8YD2akmUeRW7qxRWC2VWpbIoLKMt2UtoKNuqpHiuGXGlm3qKvvo8K8oauFApbOQ68jcCw8c7vNQpCVw-kNMFIas5ki1QfGgMLVQHs-PEuHOtFZUE-7VW3-SNGQ7Lmpm06DkrlanJVDZHlmQj7RzBKCpYFWLylnYgy5sZfo8ZOwsDSexGufBOgAAAAE2R3qpAA", default=None)
+FORCESUB = config("amazon_flipkart_meesho_dealsZone", default=None)
+AUTH = config("5205621417", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
